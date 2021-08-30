55338f3fc665bf1b518764296e31f1c8a3d04809
Nanase.E <nanase.matsui@gmail,com>
2021-08-30 11:16:38 +0200

    Update files

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,11 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import Playlist from '../Playlist/Playlist'
+import SearchResults from '../SearchResults/SearchResults';
+
+
 
 class App extends React.Component {
   render() {
@@ -8,10 +13,10 @@ class App extends React.Component {
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-         {/* Add a SearchBar component */}
+         <SearchBar />
           <div className="App-playlist">
-          {/* Add a SearchResults component  */}
-          {/* Add a Playlist component */}
+          <SearchResults />
+          <Playlist />
           </div>
       </div>
     </div>
